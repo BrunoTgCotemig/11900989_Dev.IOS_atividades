1dd4c683b8ea543aaf3ff648a1681b73320aca51
Bruno T. Galvao <11900989@aluno.cotemig.com.br>
2021-03-01 10:47:53 -0300

    activity wireframe

---
+++ 01-03-2021_atividade1.swift
@@ -0,0 +1,63 @@
+4copyPaste: fileName = 01-03-2021_atividade1.swift
+DevMobile: IOS
+
+
+
+
+Atividade 1º
+
+
+
+Por:
+
+Bruno Teles Galvao;
+3b1; n º 10;
+
+
+
+
+
+
+
+
+
+
+=================
+Questão 1 - 
+Da forma mais longa para a mais curta maneira de escrever, declare variáveis
+em um código Swift.
+=================
+
+
+
+
+=================
+Questão 2 -
+Ao pensar em um sistema de venda de carros, ao cadastrar um novo para
+venda pense em quais informações seriam constantes e quais seriam variáveis, e
+declare esses dados em um código Swift.
+=================
+
+
+
+
+=================
+Questão 3 - 
+Descreva seu entendimento sobre anotação de tipos no Swift.
+=================
+
+
+
+
+=================
+Questão 4 - 
+Declare uma variável do tipo Int e outra do tipo Double, e converta as duas para
+um texto do tipo String em uma terceira variável.
+=================
+
+
+
+=================
+Questão 5 - 
+Defina os conceitos de Type Safety e Type Inference.
+=================
