9779290dac09fe1b04541d77091182b3b35ed318
Bruno T. Galvao <11900989@aluno.cotemig.com.br>
2021-03-08 11:31:16 -0300

    +atv2: wireframe done

---
+++ Atividade 2 Swift _ 03-08/Atividade 2.swift
@@ -0,0 +1,78 @@
+4copyPaste filename: Atividade 2.swift
+
+
+
+
+
+DevMobile: IOS
+
+
+
+
+Atividade 2
+
+
+
+Por:
+
+Bruno Teles Galvao;
+3b1; n º 10;
+
+
+
+
+
+
+
+
+
+
+=================
+Questão 1 - 
+
+
+Resposta:
+```
+
+```
+=================
+
+
+
+
+=================
+Questão 2 -
+
+
+Resposta:
+ ```
+
+ ```
+=================
+
+
+
+
+=================
+Questão 3 - 
+
+
+Resposta:
+ 
+
+ 
+=================
+
+
+
+
+=================
+Questão 4 - 
+
+
+Resposta:
+```
+
+
+``` 
+=================
