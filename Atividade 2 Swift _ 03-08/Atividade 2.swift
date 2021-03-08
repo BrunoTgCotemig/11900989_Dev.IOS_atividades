a39839509d89aadbc3a55b077cf65dcf74ebaf04
Bruno T. Galvao <11900989@aluno.cotemig.com.br>
2021-03-08 15:44:38 -0300

    atv2: half, break due outside factors

--- Atividade 2 Swift _ 03-08/Atividade 2.swift
+++ Atividade 2 Swift _ 03-08/Atividade 2.swift
@@ -28,13 +28,17 @@ Bruno Teles Galvao;
 
 
 =================
-Questão 1 - 
+Questão 1 -
+     Descreva a diferença entre um if e o guard no Swift.
 
 
 Resposta:
-```
 
-```
+A expressão “guard” na linguagem swift é usada para executar uma linha de código caso a validação 
+- que está entre parênteses - retorne um resultado booleano “false”. Ela se difere de sua contrapartida 
+“if”, principalmente - mas não limitado a  - pela sua capacidade de interação quando usado dentro
+de funções, como: retornar um valor mais cedo ou ‘jogar’ um valor de erro durante a execução.
+
 =================
 
 
@@ -42,6 +46,9 @@ Resposta:
 
 =================
 Questão 2 -
+    Escreva uma função que lança dados, ela não recebe nenhum parâmetro, mas
+retorna os números lançados usando apenas uma variável, (Retorna dois valores
+utilizando apenas uma variável)
 
 
 Resposta:
