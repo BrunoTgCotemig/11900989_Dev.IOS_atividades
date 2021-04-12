1bb3003e5116206db1d3afb95fd3f20cc4b09ede
Bruno T. Galvao <11900989@aluno.cotemig.com.br>
2021-04-12 14:10:10 -0300

    +Activity: loops - stage II

--- 04-12 - Atividade Loops de repeticao/Atividade.Loops_de _repeticao.swift
+++ 04-12 - Atividade Loops de repeticao/Atividade.Loops_de _repeticao.swift
@@ -68,12 +68,43 @@ Bruno Teles Galvao;
 //==========
 //Questão 06
 
+func q_06()
+{//start
+    func ifPrimeNumber(_Number: Int) -> Bool {   if(Number % 2 == 1){return true} else {return false}   }
+
+
+    var _Array:[Int] = []
+
+
+    var dummy:Int = 93
+    while(92...1478 ~= dummy)
+    {
+        if ( ifPrimeNumber(_Number: dummy) == true ) {   _Array.append(dummy);   }   
+        dummy = dummy+1;
+    }
+
+    for i in _Array {   print(_Array[i])   }
+}//end
+
+
 //==========
 
 
 
 //==========
 //Questão 07
+
+func q_07()
+{//start
+
+    var _Array:[Int] = []
+    
+
+    for i in 0...9 {   _Array.append(Int.random(in: 0...10))    }
+    _Array.sort()
+    for i in _Array{   print(_Array[i])   }
+}//end
+
 
 //==========
 
